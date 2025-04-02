def60c4058ab3f01b74d35e4960d1b59a924d754
airimizuno <airimizuno@localhost>
2025-04-02 18:07:33 +0900

    java1.1から課題の進め方と調べ方まで完了しました！

---
+++ HelloWorld_AiriMizuno/src/sample/SelfIntroduction.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		String name = "水野　亜衣梨";
+		//年齢
+		int age = 26;
+		//趣味
+		String hobby = "映画鑑賞";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
+		System.out.println( + (10 + 5));
+		System.out.println("計算結果:" + (10 + 5));
+		
+		System.out.println("10" + 5);
+		System.out.println("文字列としての計算:" + 10 + 5);
+	}
+}
