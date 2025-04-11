39205a35876f0ed3dab6343052af6163b8c85e4d
airimizuno <airimizuno@localhost>
2025-04-11 17:36:12 +0900

    課題1.19再提出

--- HelloWorld_AiriMizuno/src/basicClass/Dog.java
+++ HelloWorld_AiriMizuno/src/basicClass/Dog.java
@@ -24,24 +24,4 @@ public class Dog {
     public int getCount() {
         return count;
     }
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-// Q1：フィールドに動物の名前の変数を定義してください。
-		
-// Q2：フィールドに動物の数の変数を定義してください。
-		
-// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-		
-// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-		// デフォルトコンストラクタ（nameに"犬"が入る）
-		Dog dog1 = new Dog();
-		System.out.println("動物の名前：" + dog1.name);
-
-        // 引数つきコンストラクタ（countに数字が入る）
-		Dog dog2 = new Dog(3);
-		System.out.println("動物の数：" + dog2.count);
-		
-	}
-
 }
